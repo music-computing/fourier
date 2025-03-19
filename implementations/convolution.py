c8753b94eeb7d5be0009ba6ad66fcbc436fa5db0
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2025-03-19 21:16:12 +0000

    more consistent: `vector` and `tuple`

--- implementations/convolution.py
+++ implementations/convolution.py
@@ -14,40 +14,41 @@ __author__ = "Anonymous for blind submission"
 # ------------------------------------------------------------------------
 
 def convolve(
-        list_1: list | tuple,
-        list_2: list | tuple,
+        vector_1: list | tuple,
+        vector_2: list | tuple,
         eliminate_doubling: bool = False
-) -> list:
+) -> tuple:
     """
     Convolution combines two vectors,
     with a scalar product of corresponding entries
     where they are rotated relative to one another by k steps.
+    If the `eliminate_doubling` is True, return only 1s and 0s.
 
     >>> c_major_triad = (1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0)
     >>> min_7_dyad = (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0)
     >>> convolve(c_major_triad, min_7_dyad)
-    [1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
+    (1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0)
 
     """
-    number_elements = len(list_1)
-    if len(list_2) != number_elements:
+    number_elements = len(vector_1)
+    if len(vector_2) != number_elements:
         raise ValueError("The lengths of and b must match.")
 
     combined_list = []
     for k in range(number_elements):
         combined_list.append(
             sum(
-                [list_1[i] * list_2[k - i % number_elements] for i in range(number_elements)]
+                [vector_1[i] * vector_2[k - i % number_elements] for i in range(number_elements)]
             )
         )
 
     if eliminate_doubling:
         combined_list = [1 if x > 0 else 0 for x in combined_list]
-    return combined_list
+    return tuple(combined_list)
 
 
 def get_interval_vector(
-        set_of_integers: list,
+        vector: list | tuple,
         return_counts: bool = True
 ) -> tuple:
     """
@@ -55,13 +56,13 @@ def get_interval_vector(
     the complete set of intervals between items within that set.
 
     Args:
-    set_of_integers (list): A list of integers.
+    vector (list): A list of integers.
     return_counts (bool): If True, returns a tuple with the counts for each integer from 0 to 6.
         (This corresponds to the standard representation in music theory.)
         Otherwise, returns a list of integers.
 
     Returns:
-    list or tuple: A list of integers or a tuple with the counts for each integer.
+        tuple: A tuple with the counts for the number of times the interval of that index position occurs.
 
     >>> test_list = [1, 3, 4, 6]
     >>> get_interval_vector(test_list, return_counts=False)
@@ -73,9 +74,9 @@ def get_interval_vector(
     """
     differences = []
 
-    for i in range(len(set_of_integers)):
-        for j in range(i + 1, len(set_of_integers)):
-            diff = abs(set_of_integers[j] - set_of_integers[i])
+    for i in range(len(vector)):
+        for j in range(i + 1, len(vector)):
+            diff = abs(vector[j] - vector[i])
             differences.append(diff)
 
     if return_counts:
@@ -85,22 +86,21 @@ def get_interval_vector(
 
 
 def interval_function(
-        start_set: list,
-        end_set: list,
+        vector_1: list | tuple,
+        vector_2: list | tuple,
         return_counts: bool = True
 ) -> tuple:
     """
-    Generates all intervals between two sets of integers and returns the difference between the two values.
+    All intervals between two vectors.
     The interval _function_ was introduced to music theory by Lewin, 2001
 
-
     Args:
-        start_set (list): A list of integers representing the start of the intervals.
-        end_set (list): A list of integers representing the end of the intervals.
-        return_counts (bool). Re-organise the list as in the interval vector.
+        vector_1 (list): A vector that may represent a starting condition.
+        vector_2 (list): A vector that may represent a following condition.
+        return_counts (bool). Re-organise the list of intervals as described for the interval vector.
 
     Returns:
-    list: A list of integers, where each integer represents the difference between the start and end of an interval.
+        tuple
 
     >>> start_set = [1, 2, 3]
     >>> end_set = [3, 4, 5]
@@ -112,8 +112,8 @@ def interval_function(
 
     """
     differences = []
-    for start in start_set:
-        for end in end_set:
+    for start in vector_1:
+        for end in vector_2:
             if start <= end:  # Ensure start is less than or equal to end
                 differences.append(end - start)
 
@@ -124,7 +124,7 @@ def interval_function(
 
 
 def values_to_counts(
-        set_of_integers: list | tuple,
+        vector: list | tuple,
         max_val: int | None = 6
 ) -> tuple:
     """
@@ -140,9 +140,9 @@ def values_to_counts(
 
     """
     if not max_val:
-        max_val = max(set_of_integers)
+        max_val = max(vector)
     counts = [0] * (max_val + 1)
-    for difference in set_of_integers:
+    for difference in vector:
         counts[difference] += 1
     return tuple(counts)
 
