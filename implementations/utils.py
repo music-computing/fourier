3e48faaa09df0d75f6a00165967ec6bd272a8746
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2025-04-20 16:02:37 +0100

    split `vectors_sets` from `utils`

--- implementations/utils.py
+++ implementations/utils.py
@@ -2,114 +2,22 @@
 """
 Demonstrate Fourier equivalences.
 
-Basic, shared functionality.
-Arguments are typically named simple `input_list`.
-We typically use this for lists of integers,
- but these function does not require this and will accept any list,
- in many cases accepting even any objects.
- Typing hints indicate any constraints.
+Additional, basic, shared functionality beyond that provided in `vectors_sets`.
 
 Source: [redacted]
 """
 
 from __future__ import annotations
+from typing import Iterable
 
 __author__ = "Anonymous for blind submission"
 
 import numpy as np
 
 
-def fourier_magnitudes(input_list: list) -> list:
+def fourier_magnitudes(input: Iterable) -> np.array:
     """Convenience function to ensure consistent handling of Fourier magnitude."""
-    return np.absolute(np.fft.fft(input_list))
-
-
-def rotate(
-        input_list: list[int],
-        n: int | None = None
-) -> list:
-    """
-    Rotate a list by N steps.
-
-    :param input_list: Any list.
-    :param n: how many steps to rotate.
-        Or, equivalently, the nth index of the input list becomes the 0th index of the new.
-        If unspecified, use the half cycle: int(<cycle lenth>/2).
-
-    >>> rotate([0, 1, 2, 3])
-    [2, 3, 0, 1]
-
-    """
-    if not n:
-        n = int(len(input_list) / 2)
-        assert n > 1
-
-    return input_list[n:] + input_list[:n]
-
-
-def mirror(
-        input_list: list | tuple,
-        index_of_symmetry: int | None = None
-) -> list:
-    """
-    Reverse a list.
-
-    Args:
-    input_list: Any list.
-    index_of_symmetry: Defaults to None, in which case, standard refelction [::-1].
-        Alternatively, specify an index to rotate about, e.g., for the reverse function in convolution use 0.
-        This is equivalent to mirror and rotation.
-        See notes at `rotate`.
-
-    >>> test_case = [0, 1, 2, 3, 4, 5]
-    >>> mirror(test_case)
-    [5, 4, 3, 2, 1, 0]
-
-    >>> mirror(test_case, index_of_symmetry=0)
-    [0, 5, 4, 3, 2, 1]
-
-    >>> mirror(test_case, index_of_symmetry=1)
-    [1, 0, 5, 4, 3, 2]
-    """
-    if index_of_symmetry is not None:
-        return input_list[index_of_symmetry::-1] + input_list[-1:index_of_symmetry:-1]
-    else:
-        return input_list[::-1]
-
-
-def multiply(
-        input_list: list,
-        scale_factor: int = 2
-) -> list:
-    """
-    Multiply all values of a list.
-
-    Args:
-    input_list: Any list.
-    scale_factor: The "scale factor" aka "multiplicative operand". Multiply all terms by this amount. Defaults to 2.
-
-    >>> multiply([0, 1, 2])
-    [0, 2, 4]
-
-    """
-    return [x * scale_factor for x in input_list]  # TODO np would be better here.
-
-
-def is_indicator_vector(indicator_vector: list | tuple) -> bool:
-    """Check that a list or tuple is an indicator vector, featuring only 0s and 1s."""
-    if all(x in (0, 1) for x in indicator_vector):
-        return True
-
-
-def complement(indicator_vector: list) -> list:
-    """
-    Provide the complement of an indicator vector.
-    >>> complement([1, 0, 1, 0])
-    [0, 1, 0, 1]
-    """
-    if not is_indicator_vector(indicator_vector):
-        raise ValueError("This is to be called only on binary lists (indicator vectors).")
-    return [1 - x for x in indicator_vector]
+    return np.absolute(np.fft.fft(input))
 
 
 def items_to_differences(
@@ -179,73 +87,19 @@ def interval_to_interval_class(interval: int) -> int:
     return (abs(interval) % 12) if (abs(interval) % 12) <= 6 else (12 - (abs(interval) % 12))
 
 
-def set_to_vector(
-        input_set: list[int] | tuple[int],
-        min_index: int | None = 0,
-        max_index: int | None = 6
-) -> tuple:
+def interval_vector_to_interval_class_vector(interval_vector: tuple[int, ...]) -> tuple[int, ...]:
     """
-    Converts any "set" (list of integers) into a "vector" (count of integers organised by index).
-    See the paper for full definitions.
-    This is similar to the collections.Counter function, simply returning an ordered list instead of a dict.
+    Map an interval vector (length 0-11) to an interval class vector (1-6).
 
-    Args:
-        input_set: The input integers.
-        min_index: The minimum index to use. Defaults to 0. Use 1 for interval vectors, to exlcude 0.
-        max_index: The maximum index to use. Defaults to 6 for interval vectors.
-
-    >>> test_set = [1, 2, 3]
-    >>> set_to_vector(test_set, min_index=0, max_index=6)
-    (0, 1, 1, 1, 0, 0, 0)
-
-    >>> set_to_vector(test_set, min_index=1, max_index=6)
-    (1, 1, 1, 0, 0, 0)
-
-    >>> set_to_vector(test_set, max_index=None)
-    (0, 1, 1, 1)
-
+    >>> interval_vector_to_interval_class_vector((1, 0, 5, 7, 2, 2, 0, 3, 5, 0, 8, 4))
+    (4, 13, 7, 7, 5, 0)
     """
-    if not max_index:
-        max_index = max(input_set)
-    counts = [0] * (max_index + 1)
-    for item in input_set:
-        counts[item] += 1
+    interval_class_vector = [0] * 6
+    for i in range(1, 6):
+        interval_class_vector[i - 1] = interval_vector[i] + interval_vector[12 - i]
 
-    if min_index > 0:
-        counts = counts[min_index:]
-
-    return tuple(counts)
-
-
-def vector_to_set(
-        vector: list[int] | tuple[int]
-) -> tuple:
-    """
-    Converts any "vector" (count of integers organised by index)
-    to a corresponding "set" (unordered list of integers).
-    See the paper for full definitions.
-
-   Args:
-       vector: The input vector.
-
-   Returns:
-       A tuple representing the set.
-
-    Examples:
-    >>> test_vector = (0, 3, 2, 1, 0, 0, 0)
-    >>> resulting_set = vector_to_set(test_vector)
-    >>> resulting_set
-    (1, 1, 1, 2, 2, 3)
-
-    >>> roundtrip = set_to_vector(resulting_set, max_index=6)
-    >>> roundtrip
-    (0, 3, 2, 1, 0, 0, 0)
-
-    >>> roundtrip == test_vector
-    True
-
-    """
-    return tuple(i for i in range(len(vector)) for _ in range(vector[i]))
+    interval_class_vector[5] = interval_vector[6]  # special case
+    return tuple(interval_class_vector)
 
 
 # ------------------------------------------------------------------------
---
+++ implementations/vectors_sets.py
@@ -0,0 +1,392 @@
+# coding: utf-8
+"""
+Basic, shared functionality for sets and vectors.
+We aim for clear and consistent use of "set" and "vector" in naming and documentation.
+We also use Python classes to that effect where practical,
+though we use some basic classes in preference over non-standard options:
+- sets are encoded as `set()`
+- multi-sets are tuples or `Counter` objects (NB: not the Python `multiset` class)
+- vectors (whether indicator or weighted): are tuples (here as elsewhere we aim for immutability)
+- ambiguous cases tend to be tuples, likewise for immutability.
+
+Argument names are "set" or "vector" where that is the clear expectation and reason for the function.
+Where the function could be more widely applied, arguments are named accordingly.
+"""
+
+__author__ = "Anonymous for blind submission"
+
+from typing import Iterable, Union
+
+
+def multiset_to_vector(
+    multiset: Iterable,
+    max_index: Union[int, None] = None,
+) -> tuple:
+    """
+    Converts any "set" or "multiset" (any iterable object containing only integers)
+    into a "vector" (count of those integers organized by index).
+    This is similar to the collections.Counter function, simply returning an ordered tuple instead of a dict.
+
+    Parameters
+    ----------
+    multiset : Iterable
+        The input integers as an iterable object (list, tuple, set).
+        Multisets are accepted but not required (i.e., sets as trivial cases of multisets).
+    max_index: Union[int, None]
+        Sets the maximum index of the output vector.
+        If None, use the maximum value of the input set.
+
+    Returns
+    -------
+    tuple: The corresponding vector.
+
+    Examples
+    --------
+    >>> test_multi_set = (1, 1, 1, 2, 2, 3)
+    >>> vector = multiset_to_vector(test_multi_set)
+    >>> vector
+    (0, 3, 2, 1)
+
+    >>> vector_with_padding = multiset_to_vector(test_multi_set, max_index=6)
+    >>> vector_with_padding
+    (0, 3, 2, 1, 0, 0, 0)
+
+    >>> roundtrip = vector_to_multiset(vector)
+    >>> roundtrip
+    (1, 1, 1, 2, 2, 3)
+
+    >>> roundtrip == test_multi_set
+    True
+
+    >>> test_set = (1, 2, 3)
+    >>> vector_2 = multiset_to_vector(test_set)
+    >>> vector_2
+    (0, 1, 1, 1)
+
+    >>> set_roundtrip = vector_to_multiset(vector_2)
+    >>> set_roundtrip
+    (1, 2, 3)
+
+    >>> set_roundtrip == test_set
+    True
+
+    """
+    if max_index is None:
+        max_index = max(multiset)
+    counts = [0] * (max_index + 1)
+    for item in multiset:
+        counts[item] += 1
+
+    return tuple(counts)
+
+
+def vector_to_multiset(vector: tuple[int, ...]) -> tuple:
+    """
+    Converts any "vector" (count of integers organised by index)
+    to a corresponding "multiset" (unordered integers).
+
+    Parameters
+    ----------
+    vector: The input vector.
+
+    Returns
+    -------
+    tuple: The corresponding set as a tuple (because it will often be a multiset).
+
+    Examples
+    --------
+    >>> test_vector = (0, 3, 2, 1, 0, 0, 0)
+    >>> resulting_set = vector_to_multiset(test_vector)
+    >>> resulting_set
+    (1, 1, 1, 2, 2, 3)
+
+    >>> roundtrip = multiset_to_vector(resulting_set, max_index=6)
+    >>> roundtrip
+    (0, 3, 2, 1, 0, 0, 0)
+
+    >>> roundtrip == test_vector
+    True
+
+    """
+    return tuple(i for i, count in enumerate(vector) for _ in range(count))
+
+
+# Arithmetic operations: Addition/subtraction, multiplication, division
+
+
+def apply_constant(
+    set_or_vector: Iterable,
+    constant: float,
+    modulo: Union[int, None] = None,
+) -> Union[tuple, list, set]:
+    """
+    Apply a constant value to a set or vector.
+
+    Parameters
+    ----------
+    set_or_vector:
+        An iterable representing a set or vector.
+    constant:
+        The constant may be an int or a float and positive or negative.
+    modulo:
+        An optional integer.
+        If provided, the result will be taken modulo this value.
+
+    Returns
+    -------
+    tuple, list, or set:
+        The set or vector with the constant applied.
+        This return type matches the input type.
+
+    Examples
+    --------
+    >>> start = (0, 1, 2, 3, 11)
+    >>> more = apply_constant(start, 1)
+    >>> more
+    (1, 2, 3, 4, 12)
+
+    >>> less = apply_constant(more, -1)
+    >>> less
+    (0, 1, 2, 3, 11)
+
+    >>> start == less
+    True
+
+    >>> more = apply_constant(start, 1, modulo=12)
+    >>> more
+    (1, 2, 3, 4, 0)
+
+    >>> as_list = [0, 1, 2, 3, 11]
+    >>> more_list = apply_constant(as_list, 1)
+    >>> more_list
+    [1, 2, 3, 4, 12]
+
+    >>> as_set = {0, 1, 2, 3, 11}
+    >>> more_set = apply_constant(as_set, 1)
+    >>> more_set
+    {1, 2, 3, 4, 12}
+    """
+    result = [x + constant for x in set_or_vector]
+    if modulo is not None:
+        result = [x % modulo for x in result]
+
+    if isinstance(set_or_vector, tuple):
+        return tuple(result)
+    elif isinstance(set_or_vector, list):
+        return result
+    elif isinstance(set_or_vector, set):
+        return set(result)
+    else:
+        return result
+
+
+def scalar_multiply(input: tuple, scale_factor: int = 2) -> tuple:
+    """
+    Multiply all values of a tuple.
+
+    Parameters
+    ----------
+    input: tuple
+
+    scale_factor: int
+        The "scale factor" aka "multiplicative operand".
+        Multiply all terms by this amount.
+        Defaults to 2.
+
+    >>> scalar_multiply((0, 1, 2))
+    (0, 2, 4)
+
+    """
+    return tuple(
+        x * scale_factor for x in input
+    )  # TODO np would be better in cases like this)
+
+
+# Transformations
+
+
+def rotate(
+    vector: Union[tuple[int, ...], list[int]], steps: Union[int, None] = None
+) -> list:
+    """
+    Rotate a vector by N steps.
+    This serves equivalently for
+    "phase shifting" of rhythm and
+    "transposition" of pitch.
+
+    Parameters
+    ----------
+    vector : Union[tuple[int, ...], list[int]]
+        Any tuple or list of any elements.
+        We expect to work with a list of integers representing a vector.
+    steps: how many steps to rotate.
+        Or, equivalently, the nth index of the input list becomes the 0th index of the new.
+        If unspecified, use the half cycle: int(<cycle lenth>/2).
+
+    Returns
+    -------
+    tuple: The input (tuple or list), rotated. Same length.
+
+    Examples
+    --------
+
+    >>> start = (0, 1, 2, 3)
+    >>> rotate(start, 1)
+    (1, 2, 3, 0)
+
+    >>> rotate(start, -1)
+    (3, 0, 1, 2)
+
+    >>> rotate(start) # note no steps specified
+    (2, 3, 0, 1)
+
+    """
+    if not steps:
+        steps = int(len(vector) / 2)
+
+    return vector[steps:] + vector[:steps]
+
+
+def mirror(
+    vector: tuple, index_of_symmetry: Union[int, None] = None
+) -> Union[list, tuple]:
+    """
+    Reverse a vector (or any ordered iterable).
+
+    Parameters
+    ----------
+    vector: tuple
+        The tuple accepts any ordered succession of any elements.
+        We expect integers representing a vector, but do not enforce it.
+    index_of_symmetry: Union[int, None] = None
+        Defaults to None, in which case, standard reflection of the form `[::-1]`.
+        Alternatively, specify an index to rotate about, e.g., for the reverse function in convolution use 0.
+        This is equivalent to mirror and rotation.
+        See notes at `rotate`.
+
+    Returns
+    -------
+    list, tuple: The input (list or tuple), mirrored. Same length.
+
+    Examples
+    --------
+    >>> test_case = (0, 1, 2, 3, 4, 5)
+    >>> mirror(test_case)
+    (5, 4, 3, 2, 1, 0)
+
+    >>> mirror(test_case, index_of_symmetry=0)
+    (0, 5, 4, 3, 2, 1)
+
+    >>> mirror(test_case, index_of_symmetry=1)
+    (1, 0, 5, 4, 3, 2)
+
+    """
+    if index_of_symmetry is not None:
+        rotated = vector[index_of_symmetry::-1] + vector[-1:index_of_symmetry:-1]
+    else:
+        rotated = vector[::-1]
+    return tuple(rotated)
+
+
+def is_set(input: Iterable) -> bool:
+    """
+    Check whether an iterable object is
+    a set (specified in the type) or
+    a de facto set (not in type, but with no repeated elements).
+
+    >>> clear_set = {1, 2, 3}
+    >>> is_set(clear_set)
+    True
+
+    >>> de_facto_set = (1, 2, 3)
+    >>> is_set(de_facto_set)
+    True
+
+    >>> de_facto_multi_set = (1, 1, 2, 3)
+    >>> is_set(de_facto_multi_set)
+    False
+    """
+    if isinstance(input, set):
+        return True
+    elif len(set(input)) == len(input):
+        return True
+    return False
+
+
+def is_indicator_vector(vector: tuple) -> bool:
+    """
+    Check whether an input vector (tuple) is an indicator vector, featuring only 0s and 1s.
+    """
+    if all(x in (0, 1) for x in vector):
+        return True
+    return False
+
+
+def multiset_to_set(multiset: Iterable):
+    """
+    For completeness, this function provides a simple mapping from multiset (iterable) to set (set).
+    No reciprocal function is possible.
+    See `weighted_to_indicator` for the corresponding treatment of vectors.
+    """
+    return set(multiset)
+
+
+def weighted_to_indicator(weighted_vector: tuple, threshold: float = 0.0) -> tuple:
+    """
+    Converts a weighted vector to an indicator vector.
+
+    Parameters
+    ----------
+    weighted_vector: tuple
+        Represents the weighted vector.
+    threshold: float
+        Values below this threshold will be set to 0.
+        This handles cases where weights might be very small but not exactly zero.
+
+    Returns
+    -------
+    tuple
+        Representing the indicator vector (0s and 1s).
+
+    Examples
+    --------
+    >>> weighted_vector1 = (0.0, 0.0, 2.0, 0.0)
+    >>> weighted_to_indicator(weighted_vector1)
+    (0, 0, 1, 0)
+
+    >>> weighted_vector2 = (0.2, 0.0, 1.5, 0.0, 0.01)
+    >>> weighted_to_indicator(weighted_vector2)
+    (1, 0, 1, 0, 1)
+
+    >>> weighted_to_indicator(weighted_vector2, threshold=0.1)
+    (1, 0, 1, 0, 0)
+    """
+    indicator_vector = []
+    for weight in weighted_vector:
+        if weight > threshold:
+            indicator_vector.append(1)
+        else:
+            indicator_vector.append(0)
+    return tuple(indicator_vector)
+    # TODO consider np.where(weighted_vector > threshold, 1, 0)
+
+
+def complement(indicator_vector: tuple[int, ...]) -> tuple:
+    """
+    Provide the complement of an indicator vector.
+    >>> complement((1, 0, 1, 0))
+    (0, 1, 0, 1)
+    """
+    if not is_indicator_vector(indicator_vector):
+        raise ValueError(
+            "This is to be called only on binary tuples representing indicator vectors."
+        )
+    return tuple(1 - x for x in indicator_vector)
+
+
+# ------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
